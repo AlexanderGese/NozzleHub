c5dd7f552f8c641a63c4a1440bd917dd931cc22f
Alexander Gese <alexander.gese07@gmail.com>
2025-05-10 17:32:47 +0200

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   preview: {
-    allowedHosts: ['all','tc8cc80w0g4k4w8ggsg80wwk.cloudcircut.tech'],
+    allowedHosts: ['all','nozzlehub.cloudcircut.tech'],
     host: true,
     port: 4321
   }
-  preview.allowedHosts: ['tc8cc80w0g4k4w8ggsg80wwk.cloudcircut.tech']
 })
